6e88dc1d8067de6513e4b4e552d020ae2a036a4d
Dylan-Ryal <DylanJRyal@gmail.com>
2021-08-21 14:11:39 +0100

    comment code and improve README

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Generates a randomized message from a bank of fixed components. Used as practice
 
 ## Technology
 + Node.js 14.16.0
+
+## Features
++ Logs a random message composed of components from a hard coded set.
+
+## Usage
+Run the `main.js` file in a command line using `node main.js`. Require **node.js** installation.
+
+## Project Status
+*Compelete*.
+
+## Improvements
++ Read words from a dictionary.
++ Output set number of sentences / sentence length.
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//Chooses a random component
 const selectComponent = componentArray => componentArray[Math.floor(Math.random() * componentArray.length)];
 
+//Selects a random component for each chunk and returns the concatenated string as a message
 const generateMessage = componentArrays => {
     let message = '';
     for (const array of componentArrays) {
@@ -8,7 +10,9 @@ const generateMessage = componentArrays => {
     return message;
 };
 
+//Initialize a 2D array to store all components for each chunk
 const messageComponents = [['Oh my, ', "It's terrible,", "I can't believe it, ", "Stop, "], ['the oven ', 'your mother ', 'the train ', 'the goverment '], 
 ['is on fire.', 'is here.', 'has crashed.', 'is attacking.']];
 
+//Log the final message to the console
 console.log(generateMessage(messageComponents));
